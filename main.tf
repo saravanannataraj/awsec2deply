cbba9d7a1222920bd0707fe353fa80608536ef56
saravanan n <saravanannataraj@hotmail.com>
2024-10-07 22:25:32 +0530

    add s3 statefile location2

--- main.tf
+++ main.tf
@@ -1,17 +1,10 @@
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    backend "s3" {
-        bucket  = "saravananterraformstate"
-        encrypt = true
-        key     = "terraform.tfstate"    
-        region  = "ap-south-1"  
-    }
+  backend "s3" {
+    bucket = "saravananterraformstate"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
   }
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
