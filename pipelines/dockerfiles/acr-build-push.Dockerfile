3d4e6dd6b24f4f353cf9cdc9422b38136562890c
saravanan n <saravanannataraj@hotmail.com>
2024-10-07 15:20:54 +0530

    cleaned old files

--- pipelines/dockerfiles/acr-build-push.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:latest
-LABEL Maintainer="Gerasimos Alexiou"
-WORKDIR /usr/app/src
-COPY python/print_datetime.py ./
-CMD [ "python", "./print_datetime.py"]
--- pipelines/python/print_datetime.py
+++
@@ -1,5 +0,0 @@
-import datetime
-x = datetime.datetime.now().strftime("%Y-%m-%d")
-print("-------- Start of python script ----------")
-print("CURRENT DATE:",x) 
-print("-------- End of python script ----------")
